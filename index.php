698e5a8fd8182902c9debfd6d0c1cdf4c2ae233e
Dmitry Tsapik <dmitry.tsapik@gmail.com>
2015-08-01 20:50:48 +0300

    Менюшка со статьями

--- index.php
+++ index.php
@@ -33,6 +33,46 @@
             }
             }
     }
+    function do_popular() {
+        $files = scandir("content/popular/");
+        unset($files[0], $files[1]);
+        arsort($files);
+        $files = array_values($files);
+        foreach($files as $event) { 
+            if (!file_exists("content/popular/" . $event)) continue;
+                $counter = 0;
+                $handle = fopen("content/popular/" . $event, "r");
+                if ($handle) {
+                //echo "<div class=\"event\">\n";
+                while (($buffer = fgets($handle)) !== false) {
+                $buffer_short = substr($buffer, 0, -2);
+                switch ($counter) {
+                    case 0:
+                        echo "\n<a href=\"" . $buffer_short . "\">";
+                        break;
+                    case 1:
+                        echo "\n<img src=\"" . $buffer_short . "\"";
+                        break;
+                    case 2:
+                        echo " alt=\"" . $buffer_short . "\">";
+                        break;
+                    case 3:
+                        echo "\n<h4>" . $buffer_short . "</h4>";
+                        break;
+                    case 4:
+                        echo "\n" . $buffer . "\n</a>";
+                        break;
+                }
+                $counter++;
+                }
+                /*if (!feof($handle)) {
+                echo "Error: unexpected fgets() fail\n";
+                }*/
+                fclose($handle);
+                //echo "</div>\n</div>\n";
+            }
+            }
+    }
     include "main/header.html";
     echo "\n";
     include "main/menu.html";
@@ -71,8 +111,22 @@
 	} 
 	else
 	{
-		include("main/main.html");
+		include("main/default_header.html");
+        echo "<div class=\"events\">";
+            $loc_dir = array(array("annonce", "Анонс"), array("news", "Новости кафедр"), array("news_deps", "Объявления"));
+            foreach ($loc_dir as $category) {
+                echo "\n<div class=\"" . $category[0] . "\">
+                      \n<h2>" . $category[1] ."</h2>";
+                      do_events("content/". $category[0] ."/");
+                echo "</div>";
+            }
+            echo "</div>\n";
+            echo "<h2>Новые статьи Физико-технического института</h2>
+                    <div class=\"popular\">";
+            do_popular();
+            echo "</div>";
+        include("main/default_footer.html");
 	}
-    echo "      </article>\n";
+       echo "      </article>\n";
 	include("main/footer.html");
 ?>
