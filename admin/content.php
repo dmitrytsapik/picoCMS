0dcf21e649f3bf46b4d8888cc74c6786620437f4
Dmitry Tsapik <dmitry.tsapik@gmail.com>
2015-08-10 08:44:06 +0300

    панель администратора

---
+++ admin/content.php
@@ -0,0 +1,54 @@
+<?php
+if ($_POST) {
+  foreach ($_POST as $key => $value) {
+    switch ($key) {
+        case 'description' :
+            file_put_contents("../content/description", $value);
+            break;
+        case 'keywords' :
+            file_put_contents("../content/keywords", $value);
+            break;
+        case 'default_header' :
+            file_put_contents("../content/header", $value);
+            break;
+        case 'default_footer' :
+            file_put_contents("../content/youtube", $value);
+            break;
+        case 'footer' :
+            file_put_contents("../content/footer", $value);
+            break;
+    }
+  }
+  header ("Location: " . $_SERVER['PHP_SELF']);
+}
+$description = htmlspecialchars(file_get_contents("../content/description"));
+$keywords = htmlspecialchars(file_get_contents("../content/keywords"));
+$default_header = htmlspecialchars(file_get_contents("../content/header"));
+$default_footer = htmlspecialchars(file_get_contents("../content/youtube"));
+$footer = htmlspecialchars(file_get_contents("../content/footer"));
+?>
+<h1>Описание сайта</h1>
+<form method="POST">
+<input size=100 name="description" value="<?php echo $description; ?>">
+<input type="submit">
+</form>
+<h1>Ключевые слова</h1>
+<form method="POST">
+<textarea cols=100 rows=3 name="keywords"><?php echo $keywords; ?></textarea>
+<input type="submit">
+</form>
+<h1>Тело верх</h1>
+<form method="POST">
+<textarea cols=100 rows=10 name="default_header"><?php echo $default_header; ?></textarea>
+<input type="submit">
+</form>
+<h1>Youtube</h1>
+<form method="POST">
+<textarea cols=100 rows=10 name="default_footer"><?php echo $default_footer; ?></textarea>
+<input type="submit">
+</form>
+<h1>Тело низ</h1>
+<form method="POST">
+<textarea cols=100 rows=10 name="footer"><?php echo $footer; ?></textarea>
+<input type="submit">
+</form>
---
+++ admin/news.php
@@ -0,0 +1,92 @@
+<?php
+echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Панель инструментов администратора</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <h1 class="text-center">Панель инструментов администратора</h1>
+HTML;
+if (isset($_POST['category'])) {
+	$description = file_get_contents("../content/" . $_POST['category'] . "/" . $_POST['item']);
+	echo "<h1>Тело</h1>
+		  <form method=\"POST\">
+			\n<input type=\"hidden\" name=\"write\" value=\"" . $_POST['category'] . "\">
+			\n<input type=\"hidden\" name=\"item\" value=\"" . $_POST['item'] . "\">
+			<textarea class=\"form-control\" cols=100 rows=10 name=\"main\">" . $description . "</textarea>
+			<input type=\"submit\">
+		  </form>";
+	exit;
+}
+if (isset($_POST['write'])) {
+	file_put_contents("../content/" . $_POST['write'] ."/" . $_POST['item'], $_POST['main']);
+  	header ("Location: ".$_SERVER['PHP_SELF']);
+	exit;
+}
+if (isset($_POST['empty'])) {
+	$empty_content = "Месяц<br>число\n<a href=\"адрес ссылки\">Ссылка</a>тело новости\n14:00";
+	if(file_put_contents("../content/" . $_POST['empty'] ."/" . time() , $empty_content)) {
+echo <<<HTML
+<div class="alert alert-success alert-dismissible fade in" id="alert" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <h4>Пустая новость добавлена!</h4>
+</div>
+HTML;
+	}
+	//header ("Location: ".$_SERVER['PHP_SELF']);
+	//exit;
+}
+if (isset($_POST['delete'])) {
+	unlink("../content/" . $_POST['delete'] ."/" . $_POST['item'] );
+}
+
+	function search_event($location) {
+		$files = scandir($location);
+    	unset($files[0], $files[1]);
+    	arsort($files);
+ 		$files = array_values($files);
+  		foreach ($files as $event) {
+     		echo "<option value=". $event . ">" . $event . "</option>";
+		}
+	}
+
+	$loc_dir = array(array("annonce", "Анонс"), array("news", "Новости кафедр"), array("news_deps", "Объявления"), array("popular", "Новые статьи"));
+	foreach ($loc_dir as $category) {
+		echo $category[1] . ": ";
+		echo "\n<form method=\"POST\">
+			  \n<input type=\"hidden\" name=\"category\" value=\"" . $category[0] . "\"> 
+			  \n<select name=\"item\">";
+		search_event("../content/" . $category[0] ."/");
+		echo "\n</select>
+			  \n<input type=\"submit\">
+			\n</form>";
+		echo "\n<form method=\"POST\">
+			  \n<input type=\"hidden\" name=\"delete\" value=\"" . $category[0] . "\"> 
+			  \n<select name=\"item\">";
+		search_event("../content/" . $category[0] ."/");
+		echo "\n</select>
+			  \n<input type=\"submit\" value=\"Удалить новость в " . $category[1] ."\">
+			\n</form>";
+		echo "\n<form method=\"POST\">
+			  \n<input type=\"hidden\" name=\"empty\" value=\"" . $category[0] . "\"> 
+			  \n<input type=\"submit\" value=\"Пустая новость в " . $category[1] ."\">
+			\n</form>";
+	}
+echo <<<HTML
+</body>
+</html>
+HTML;
+?>
